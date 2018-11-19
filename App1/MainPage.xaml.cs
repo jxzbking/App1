3ee05d57cd08dd64389e5927a5aa7a3afdccc26b
Administrator <Administrator@DESKTOP-65OV963>
2018-11-19 11:18:29 +0800

    UPDATA

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,8 @@ namespace App1
         public MainPage()
         {
             this.InitializeComponent();
+            new MessageDialog("message").ShowAsync();
+            
         }
     }
 }
