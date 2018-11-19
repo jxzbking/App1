9ee78eb097f8a99428b7298ec64579d54d74a412
Administrator <Administrator@DESKTOP-65OV963>
2018-11-19 13:30:16 +0800

    up

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,11 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string a = "11111";
+            string b = "22222";
+        }
     }
 }
